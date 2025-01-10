044fc4a94f148707ca921680b732fd7d36c13e8d
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-01-10 16:28:17 +0100

    test

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // # INIT EXPRESS
 const express = require("express");
-// const cors = require('cors');
 const app = express();
-const port = process.env.HOST_PORT;
-const domain = process.env.HOST_DOMAIN;
-
-
-
-
-
+const { HOST_PORT, HOST_DOMAIN } = process.env;
 
 //*START LISTENING
-app.listen(port, () => {
-    console.log(`Server listening to ${domain}:${port}`);
-  });
+app.listen(HOST_PORT, () => {
+  console.log(`Server listening at ${HOST_DOMAIN}:${HOST_PORT}`);
+});
