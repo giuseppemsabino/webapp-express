ebe8b32a454dd51e58ca3d69ed80aba213f8663a
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-01-10 19:51:32 +0100

    setup finish

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -3,5 +3,7 @@ function index  (req,res){
     res.json({
         message: "ok"
     });
+    
+    
 }
 module.exports = {index}
--- routers/moviesRouters.js
+++ routers/moviesRouters.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const moviesController = require("../controllers/moviesController")
 
-router.get("/",moviesController.index)
+router.get("/movies",moviesController.index)
 
 module.exports = router
