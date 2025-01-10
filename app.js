cbbdfe0d85dc3bd37c7750e696d284d89cbea031
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-01-10 19:04:15 +0100

    middlewares (notfoun, errorhandler), text test and files rename

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ const { HOST_PORT, HOST_DOMAIN } = process.env;
 const express = require("express");
 const app = express();
 
+//REGISTRING MIDDLEWARES
+const errorsHandler = require("./middlewares/errorsHandler");
+const notFound = require("./middlewares/notFound")
+
+
+app.use(express.json())
+app.use(express.static('public'));
+
+
+//# ERROR HANDLER
+
+
+
 // # ROUTERS 
-const booksRouter = require("./routers/booksRouters");
-app.use(booksRouter);
+const moviesRouter = require("./routers/moviesRouters");
+app.use("/movies",moviesRouter);
 
 //*START LISTENING
 app.listen(HOST_PORT, () => {
