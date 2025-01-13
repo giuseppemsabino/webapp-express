1fe8cf6fe47f4446f737fcda454c30a61c433f7c
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-01-13 17:47:32 +0100

    cors intall for connect to ebapp-react done, connected  and working

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 require("dotenv").config();
 
-// # INIT EXPRESS
+// *INIT EXPRESS
 const { HOST_PORT, HOST_DOMAIN } = process.env;
 const express = require("express");
+const cors = require('cors')
 const app = express();
 
-// # middlewares
+// ! middlewares
+app.use(cors())
 app.use(express.json());
 app.use(express.static("public"));
 
-// # ROUTERS
+// ! ROUTERS
 const moviesRouter = require("./routers/moviesRouters");
 app.use("/api/movies", moviesRouter);
 
-// # ERROR HANDLER
+//! ERROR HANDLER
 const notFound = require("./middlewares/notFound");
 const errorsHandler = require("./middlewares/errorsHadler");
 
-// I middleware per la gestione degli errori devono essere aggiunti DOPO i router
+// *I middleware per la gestione degli errori devono essere aggiunti DOPO i router
 app.use(notFound);
 app.use(errorsHandler);
 
