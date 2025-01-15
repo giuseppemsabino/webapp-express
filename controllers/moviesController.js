15db54e857eab882d5598e122be3e189ef8d9649
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-01-15 18:25:38 +0100

    post working

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -12,24 +12,20 @@ function index(req, res) {
 
     FROM movies`;
   connection.query(sql, (err, results) => {
+    if (err) {
+      console.log(err);
 
-    if (err){
-        console.log(err);
-
-        return res.status(500).json({
-            status: "KO",
-            message: 'Database query failed'
-        })
-        
-    };
-
+      return res.status(500).json({
+        status: "KO",
+        message: "Database query failed",
+      });
+    }
 
     const movies = results.map((movie) => ({
-       ...movie,
-        image: generateCover(movie.image),
-      }));
-      console.log(movies);
-      
+      ...movie,
+      image: generateCover(movie.image),
+    }));
+    console.log(movies);
 
     res.json({
       message: "ok",
@@ -39,56 +35,56 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    const movieId= req.params.id;
-    const sqlMovie = `
+  const movieId = req.params.id;
+  const sqlMovie = `
     SELECT id, title, director, genre, release_year, image 
     FROM movies
     WHERE id = ?`;
-    connection.query(sqlMovie,[movieId], (err, results) =>{
-        if (err){
-            console.log(err);
-    
-            return res.status(500).json({
-                status: "KO",
-                message: 'Database query failed'
-            })
-            
-        };
-        const [movie] = results
-        
-        res.json({
-            message: "ok",
-            movie,
-        });
+  connection.query(sqlMovie, [movieId], (err, results) => {
+    if (err) {
+      console.log(err);
+
+      return res.status(500).json({
+        status: "KO",
+        message: "Database query failed",
+      });
+    }
+    const [movie] = results;
+
+    res.json({
+       status: "KO",
+        message: "Database query failed"
     });
-    
+  });
 }
 
-function createReview(req,res){
+function createReview(req, res) {
+  const movieId = req.params.id;
+  const { name, vote, text } = req.body;
 
   const sql = `
-  INSERT INTO reviews (movie_id, name, vote, text)
-  VALUES (?, ?, ?, ?)
+  INSERT INTO reviews (movie_id, name, vote, text) VALUES (?, ?, ?, ?);
 `;
-    connection.query(sql, [movie_id, name, vote, text], (err,results)=>{
-        if (err){
-            console.log(err);
-    
-            return res.status(500).json({
-                status: "KO",
-                message: 'Database query failed'
-            })
-            
-        };
+connection.query(sql, [movieId, name, vote, text], (err) => {
+  if (err) {
+    console.log(err);
+
+    return res.status(500).json({
+      status: "KO",
+      message: "Database query failed",
     });
+  }
 
-
+  res.json({
+    status: "ok",
+    message: "Review added",
+  });
+});
 }
 
 const generateCover = (coverName) => {
   const { HOST_PORT, HOST_DOMAIN } = process.env;
- return `${HOST_DOMAIN}:${HOST_PORT}/public/img/${coverName}`;
-  
+  return `${HOST_DOMAIN}:${HOST_PORT}/public/img/${coverName}`;
 };
 // console.log(generateCover);
 
--- routers/moviesRouters.js
+++ routers/moviesRouters.js
@@ -5,7 +5,7 @@ const moviesController = require("../controllers/moviesController")
 
 router.get("/",moviesController.index);
 router.get("/:id",moviesController.show);
-router.post("/reviews",moviesController.createReview)
+router.post("/:id/reviews",moviesController.createReview) // post http://localhost:3001/movies/:id/reviews
 // router.get("/:id",moviesController.update);
 // router.get("/:id",moviesController.delete);
 
